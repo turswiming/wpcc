c6cdc227498e0ceffb08f55559fdbf13b57596fd
liziqi <1431839116@qq.com>
2024-07-23 15:53:51 +0800

    quanti_2 level

--- testcompression.py
+++ testcompression.py
@@ -27,10 +27,13 @@ from pydub import AudioSegment
 x_real_original_global = np.array([])
 x_imag_original_global = np.array([])
 class PCcompression:
-    def __init__(self,box_size,frame_size,compression_value ,visualize = False, use8bit =False) -> None:
+    def __init__(self,box_size,frame_size,compression_value,edge_size =16,visualize = False, use8bit =False) -> None:
         self.box_size = box_size
         self.frame_size = frame_size
         self.compression_value = compression_value
+        if edge_size *2 >=frame_size:
+            raise ValueError("edge_size should be smaller than frame_size/2")
+        self.edge_size = edge_size
         self.visualize = visualize
         self.use8bit = use8bit
         pass
@@ -73,65 +76,66 @@ class PCcompression:
         for i in range(x_fft_frames_array.shape[0]):
             real[i] = x_fft_frames_array[i].real
             imag[i] = x_fft_frames_array[i].imag
-        #2.1 store max min values
+        if channel_name == "x":
+            global x_real_original_global
+            global x_imag_original_global
+            x_real_original_global = real
+            x_imag_original_global = imag
+
+        real_edge = np.zeros((x_fft_frames_array.shape[0], self.edge_size*2))
+        imag_edge = np.zeros((x_fft_frames_array.shape[0], self.edge_size*2))
+        for i in range(x_fft_frames_array.shape[0]):
+            real_edge[i] = np.concatenate((real[i][:self.edge_size], real[i][-self.edge_size:]))
+            imag_edge[i] = np.concatenate((imag[i][:self.edge_size], imag[i][-self.edge_size:]))
+        real_edge_max = np.max(real_edge)
+        real_edge_min = np.min(real_edge)
+        imag_edge_max = np.max(imag_edge)
+        imag_edge_min = np.min(imag_edge)
+        real_edge = (real_edge-real_edge_min)/(real_edge_max-real_edge_min)
+        imag_edge = (imag_edge-imag_edge_min)/(imag_edge_max-imag_edge_min)
+        if self.use8bit:
+            real_edge = (real_edge*255)
+            imag_edge = (imag_edge*255)
+        else:
+            real_edge = (real_edge*65535)
+            imag_edge = (imag_edge*65535)    
+        if self.use8bit:
+            real_edge = real_edge.astype(np.uint8)
+            imag_edge = imag_edge.astype(np.uint8)
+        else:
+            real_edge = real_edge.astype(np.uint16)
+            imag_edge = imag_edge.astype(np.uint16)
+        real_edge_pil_img = Image.fromarray(real_edge)
+        imag_edge_pil_img = Image.fromarray(imag_edge)
+        real_edge_pil_img.save(
+            "data_output/{}_saveimg/{}_fft_frames_edge_real.png".format(prefix, channel_name),
+            format="png",
+        )
+        imag_edge_pil_img.save(
+            "data_output/{}_saveimg/{}_fft_frames_edge_imag.png".format(prefix, channel_name),
+            format="png",
+        )
+        
+        #2.2 store edge values
+        real = real[:,self.edge_size:-self.edge_size]
+        imag = imag[:,self.edge_size:-self.edge_size]
         real_max = np.max(real)
         real_min = np.min(real)
         imag_max = np.max(imag)
         imag_min = np.min(imag)
-        with open("data_output/{}_saveimg/{}_fft_frames.json".format(prefix,channel_name), "w") as f:
-            json.dump({"real_max":real_max, "real_min":real_min, "imag_max":imag_max, "imag_min":imag_min}, f)
-
         real = (real-np.min(real))/(np.max(real)-np.min(real))
         imag = (imag-np.min(imag))/(np.max(imag)-np.min(imag))
-        real = pow(real, 0.5)
-        imag = pow(imag, 0.5)
+        # real = pow(real, 0.5)
+        # imag = pow(imag, 0.5)
         if self.use8bit:
             real_image = (real*255)
             imag_image = (imag*255)
         else:
             real_image = (real*65535)
             imag_image = (imag*65535)
-        if channel_name == "x":
-            global x_real_original_global
-            global x_imag_original_global
-            x_real_original_global = real_image
-            x_imag_original_global = imag_image
-        # #save to image
-        # # 获取数组每个64*64方格内最大值最小值
-        # real_min_map, real_max_map = self.get_min_max(real_image, self.box_size, self.box_size)
-        # imag_min_map, imag_max_map = self.get_min_max(imag_image, self.box_size, self.box_size)
 
-        # real_min_map_adjusted = np.repeat(real_min_map, self.box_size,axis=0)  # Adjusting the shape
-        # real_max_map_adjusted = np.repeat(real_max_map, self.box_size,axis=0)
-        # imag_min_map_adjusted = np.repeat(imag_min_map, self.box_size,axis=0)
-        # imag_max_map_adjusted = np.repeat(imag_max_map, self.box_size,axis=0)
 
-        # real_min_map_adjusted = np.repeat(real_min_map_adjusted, self.box_size,axis=1)  # Adjusting the shape
-        # real_max_map_adjusted = np.repeat(real_max_map_adjusted, self.box_size,axis=1)
-        # imag_min_map_adjusted = np.repeat(imag_min_map_adjusted, self.box_size,axis=1)
-        # imag_max_map_adjusted = np.repeat(imag_max_map_adjusted, self.box_size,axis=1)
-        # # cut real_min_map_adjusted to the shape of real_image
-        # real_min_map_adjusted = real_min_map_adjusted[:real_image.shape[0], :real_image.shape[1]]
-        # real_max_map_adjusted = real_max_map_adjusted[:real_image.shape[0], :real_image.shape[1]]
-        # imag_min_map_adjusted = imag_min_map_adjusted[:imag_image.shape[0], :imag_image.shape[1]]
-        # imag_max_map_adjusted = imag_max_map_adjusted[:imag_image.shape[0], :imag_image.shape[1]]
         
-        # real_image = (real_image - real_min_map_adjusted) / (real_max_map_adjusted - real_min_map_adjusted) * 65535
-        # imag_image = (imag_image - imag_min_map_adjusted) / (imag_max_map_adjusted - imag_min_map_adjusted) * 65535
-        # for i in range(real_image.shape[1]):
-        #     wave = real_image[:,i]
-        #     wave = wave-32767
-        #     wave = wave.astype(np.int16)
-        #     audio = AudioSegment(wave.tobytes(), frame_rate=44100, sample_width=2, channels=1)
-        #     audio.export("data_output/{}_saveimg/{}_fft_frames_real_{}.wav".format(prefix,channel_name,i), format="wav")
-            
-        # for i in range(imag_image.shape[1]):
-        #     wave = imag_image[:,i]
-        #     wave = wave-32767            
-        #     wave = wave.astype(np.int16)
-        #     audio = AudioSegment(wave.tobytes(), frame_rate=44100, sample_width=2, channels=1)
-        #     audio.export("data_output/{}_saveimg/{}_fft_frames_imag_{}.wav".format(prefix,channel_name,i), format="wav") 
-            
     
         if self.use8bit:
             real_image = real_image.astype(np.uint8)
@@ -139,30 +143,37 @@ class PCcompression:
         else:
             real_image = real_image.astype(np.uint16)
             imag_image = imag_image.astype(np.uint16)
+            
+        if channel_name == "x":
+            max_values = np.max(imag_image, axis=0)
+            min_values = np.min(imag_image, axis=0)
+            mean_values = np.mean(imag_image, axis=0)  # 计算平均值
+
+            columns = np.arange(imag_image.shape[1])
+            plt.figure(figsize=(10, 5))
+
+            plt.plot(columns, max_values, label='Max Values', marker='o', linestyle='-', color='r')
+            plt.plot(columns, min_values, label='Min Values', marker='o', linestyle='-', color='b')
+            plt.plot(columns, mean_values, label='Mean Values', marker='x', linestyle='--', color='g')  # 添加平均值
+
+            plt.title('Max, Min, and Mean Values of Each Column in imag_image')
+            plt.xlabel('Column Index')
+            plt.ylabel('Values')
+            plt.xticks(columns)
+            plt.legend()
+
+            plt.show()
+            plt.close()
+        
         # 将numpy数组转换为Pillow图像
         real_pil_image = Image.fromarray(real_image)
         imag_pil_image = Image.fromarray(imag_image)
-
-        # real_min_map = real_min_map.astype(np.uint16)
-        # real_max_map = real_max_map.astype(np.uint16)
-        # imag_min_map = imag_min_map.astype(np.uint16)
-        # imag_max_map = imag_max_map.astype(np.uint16)
-        # # 保存最大最小图像
-        # real_pil_min_image = Image.fromarray(real_min_map)
-        # real_pil_max_image = Image.fromarray(real_max_map)
-        # imag_pil_min_image = Image.fromarray(imag_min_map)
-        # imag_pil_max_image = Image.fromarray(imag_max_map)
-        # real_pil_min_image.save("data_output/{}_saveimg/{}_fft_frames_real_min.png".format(prefix, channel_name))
-        # real_pil_max_image.save("data_output/{}_saveimg/{}_fft_frames_real_max.png".format(prefix, channel_name))
-        # imag_pil_min_image.save("data_output/{}_saveimg/{}_fft_frames_imag_min.png".format(prefix, channel_name))
-        # imag_pil_max_image.save("data_output/{}_saveimg/{}_fft_frames_imag_max.png".format(prefix, channel_name))
-
 
         real_pil_image.save(
             "data_output/{}_saveimg/{}_fft_frames_real.jp2".format(prefix, channel_name),
             format="JPEG2000",
             quality_mode="rates",
-            quality_layers=[self.compression_value]  # 设置压缩率，较低的值意味着较高的压缩率
+            quality_layers=[self.compression_value*2]  # 设置压缩率，较低的值意味着较高的压缩率
         )
 
         # 保存imag部分的图像
@@ -170,35 +181,24 @@ class PCcompression:
             "data_output/{}_saveimg/{}_fft_frames_imag.jp2".format(prefix, channel_name),
             format="JPEG2000",
             quality_mode="rates",
-            quality_layers=[self.compression_value]  # 设置压缩率
+            quality_layers=[self.compression_value*2]  # 设置压缩率
         )
+        with open("data_output/{}_saveimg/{}_fft_frames.json".format(prefix,channel_name), "w") as f:
+            json.dump(
+                {
+                    "real_max":real_max, 
+                    "real_min":real_min, 
+                    "imag_max":imag_max, 
+                    "imag_min":imag_min,
+                    "real_edge_max":real_edge_max,
+                    "real_edge_min":real_edge_min,
+                    "imag_edge_max":imag_edge_max,
+                    "imag_edge_min":imag_edge_min
+                    }, 
+                f)
 
-        # 保存imag部分的图像
-        
-        # real_pil_image.save(
-        #     "data_output/{}_saveimg/{}_fft_frames_real.jpg".format(prefix, channel_name),
-        #     format="JPEG",
-        #     quality=85,  # 设置压缩率
-        # )
-            
-        # imag_pil_image.save(
-        #     "data_output/{}_saveimg/{}_fft_frames_imag.jpg".format(prefix, channel_name),
-        #     format="JPEG",
-        #     quality=85,  # 设置压缩率
-        # )
-        # plt.hist(real_image.flatten(), bins=255, color='r')
-        # plt.show()
-        # plt.close()
-        # real_pil_image.save(
-        #     "data_output/{}_saveimg/{}_fft_frames_real.png".format(prefix,channel_name),
-        #     format="png",
-        #     )
-        # imag_pil_image.save(
-        #     "data_output/{}_saveimg/{}_fft_frames_imag.png".format(prefix,channel_name),
-        #     format="png",
-        #     )
-        
 
+    
 
         
         
@@ -209,69 +209,9 @@ class PCcompression:
         real_min = json_data["real_min"]
         imag_max = json_data["imag_max"]
         imag_min = json_data["imag_min"]
+        
         real_image = imageio.imread("data_output/{}_saveimg/{}_fft_frames_real.jp2".format(prefix,channel_name))
         imag_image = imageio.imread("data_output/{}_saveimg/{}_fft_frames_imag.jp2".format(prefix,channel_name))
-        # real_waves = {}
-        # imag_waves = {}
-        # for files in os.listdir("data_output/{}_saveimg/".format(prefix)):
-        #     if files.endswith(".wav"):
-        #         audio = AudioSegment.from_file("data_output/{}_saveimg/".format(prefix)+files,format="wav")
-        #         np_audio = np.array(audio.get_array_of_samples())
-        #         index = int(files.split("_")[-1].split(".")[0])
-        #         np_audio = np_audio.astype(np.float64)
-        #         np_audio = np_audio + 32767
-        #         if files.startswith("x_fft_frames_real"):
-        #             real_waves[index] = np_audio
-        #         elif files.startswith("x_fft_frames_imag"):
-        #             imag_waves[index] = np_audio
-        
-        # real_image = np.zeros((len(real_waves[0]),len(real_waves)))
-        # imag_image = np.zeros((len(imag_waves[0]),len(imag_waves)))
-        # for key in real_waves:
-        #     real_image[:,key] = real_waves[key]
-        # for key in imag_waves:
-        #     imag_image[:,key] = imag_waves[key]
-
-        # if channel_name == "x":
-        #     diff = x_imag_original_global-imag_image
-        #     original_avg = np.mean(x_imag_original_global)
-        #     diff_avg = np.mean(diff)
-        #     now_avg = np.mean(imag_image)
-        #     print("original_avg: ", original_avg)
-        #     print("diff_avg: ", diff_avg)
-        #     print("now_avg: ", now_avg)
-        #     plt.imshow(diff)
-        #     plt.show()
-        #     plt.close()
-        # real_max_map = imageio.imread("data_output/{}_saveimg/{}_fft_frames_real_max.png".format(prefix,channel_name))
-        # real_min_map = imageio.imread("data_output/{}_saveimg/{}_fft_frames_real_min.png".format(prefix,channel_name))
-        # imag_max_map = imageio.imread("data_output/{}_saveimg/{}_fft_frames_imag_max.png".format(prefix,channel_name))
-        # imag_min_map = imageio.imread("data_output/{}_saveimg/{}_fft_frames_imag_min.png".format(prefix,channel_name))
-
-        #     # 根据最大值最小值进行压缩
-        # real_min_map_adjusted = np.repeat(real_min_map, self.box_size,axis=0)  # Adjusting the shape
-        # real_max_map_adjusted = np.repeat(real_max_map, self.box_size,axis=0)
-        # imag_min_map_adjusted = np.repeat(imag_min_map, self.box_size,axis=0)
-        # imag_max_map_adjusted = np.repeat(imag_max_map, self.box_size,axis=0)
-
-        # real_min_map_adjusted = np.repeat(real_min_map_adjusted, self.box_size,axis=1)  # Adjusting the shape
-        # real_max_map_adjusted = np.repeat(real_max_map_adjusted, self.box_size,axis=1)
-        # imag_min_map_adjusted = np.repeat(imag_min_map_adjusted, self.box_size,axis=1)
-        # imag_max_map_adjusted = np.repeat(imag_max_map_adjusted, self.box_size,axis=1)
-
-        # # cut real_min_map_adjusted to the shape of real_image
-        # real_min_map_adjusted = real_min_map_adjusted[:real_image.shape[0], :real_image.shape[1]]
-        # real_max_map_adjusted = real_max_map_adjusted[:real_image.shape[0], :real_image.shape[1]]
-        # imag_min_map_adjusted = imag_min_map_adjusted[:imag_image.shape[0], :imag_image.shape[1]]
-        # imag_max_map_adjusted = imag_max_map_adjusted[:imag_image.shape[0], :imag_image.shape[1]]
-        # real_image = real_image.astype(np.float64)
-        # imag_image = imag_image.astype(np.float64)
-        
-        # real_image = real_image * (real_max_map_adjusted - real_min_map_adjusted) / 65535 + real_min_map_adjusted
-        # imag_image = imag_image * (imag_max_map_adjusted - imag_min_map_adjusted) / 65535 + imag_min_map_adjusted
-        
-        
-        
         real_image = real_image.astype(np.float64)
         imag_image = imag_image.astype(np.float64)
         if self.use8bit:
@@ -280,10 +220,38 @@ class PCcompression:
         else:
             real_image = ((real_image)/65535)
             imag_image = ((imag_image)/65535)
-        real_image  =pow(real_image, 1/0.5)
-        imag_image  =pow(imag_image, 1/0.5)
+        # real_image  =pow(real_image, 1/0.5)
+        # imag_image  =pow(imag_image, 1/0.5)
         real_image = real_image*(real_max-real_min) + real_min
         imag_image = imag_image*(imag_max-imag_min) + imag_min
+        real_image_edge = imageio.imread("data_output/{}_saveimg/{}_fft_frames_edge_real.png".format(prefix,channel_name))
+        imag_image_edge = imageio.imread("data_output/{}_saveimg/{}_fft_frames_edge_imag.png".format(prefix,channel_name))
+        real_edge_max = json_data["real_edge_max"]
+        real_edge_min = json_data["real_edge_min"]
+        imag_edge_max = json_data["imag_edge_max"]
+        imag_edge_min = json_data["imag_edge_min"]
+        real_edge = real_image_edge.astype(np.float64)
+        imag_edge = imag_image_edge.astype(np.float64)
+        if self.use8bit:
+            real_edge = ((real_edge)/255)
+            imag_edge = ((imag_edge)/255)
+        else:
+            real_edge = ((real_edge)/65535)
+            imag_edge = ((imag_edge)/65535)
+        real_edge = real_edge*(real_edge_max-real_edge_min) + real_edge_min
+        imag_edge = imag_edge*(imag_edge_max-imag_edge_min) + imag_edge_min
+        real_image = np.concatenate((real_edge[:,:self.edge_size], real_image, real_edge[:,-self.edge_size:]), axis=1)
+        imag_image = np.concatenate((imag_edge[:,:self.edge_size], imag_image, imag_edge[:,-self.edge_size:]), axis=1)        
+        if channel_name == "x":
+            global x_real_original_global
+            global x_imag_original_global
+            
+            realdiff = x_real_original_global - real_image
+            plt.imshow(realdiff)
+            plt.show()
+            plt.close()
+            x_imag_original_global = imag_image
+        
         fft_frames = real_image + 1j*imag_image
         #reconstruct the original fft frames
         x_original_frames = []
@@ -363,21 +331,21 @@ class PCcompression:
 path = "./data_input/0001_point_cloud.ply"
 
 if __name__ == "__main__":
-    pcc = PCcompression(4096, 4,1,False,False)
+    pcc = PCcompression(4096, 512,1,16,True,False)
     pcc.pc2mp3(path)
-    pcc = PCcompression(4096, 4,2,False,False)
+    pcc = PCcompression(4096, 4,2,False,True)
     pcc.pc2mp3(path)
-    pcc = PCcompression(4096, 4,3,False,False)
+    pcc = PCcompression(4096, 4,3,False,True)
     pcc.pc2mp3(path)
-    pcc = PCcompression(4096, 4,5,False,False)
+    pcc = PCcompression(4096, 4,5,False,True)
     pcc.pc2mp3(path)
-    pcc = PCcompression(4096, 4,10,False,False)
+    pcc = PCcompression(4096, 4,10,False,True)
     pcc.pc2mp3(path)
-    pcc = PCcompression(4096, 8,20,False,False)
+    pcc = PCcompression(4096, 8,20,False,True)
     pcc.pc2mp3(path)
-    pcc = PCcompression(4096, 2048,50,False,False)
+    pcc = PCcompression(4096, 128,50,True,True)
     pcc.pc2mp3(path)
-    pcc = PCcompression(4096, 2048,100,False,False)
+    pcc = PCcompression(4096, 2048,100,False,True)
     pcc.pc2mp3(path)
     box_sizes = [128,256,512,1024]
     frame_sizes = [4,8,16,32,64,128, 256, 512,1024,2048]
